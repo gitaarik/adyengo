ee98f19ab84fe5c9e1f5a5795c7bc6c2c405dc6e
Rik <gitaarik@gmail.com>
2017-06-16 13:45:04 +0200

    made ready for PyPi

--- setup.py
+++ setup.py
@@ -1,36 +1,17 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1'
-
-REQUIREMENTS = (
-    'ipaddress==1.0.4',
-    'python-dateutil==2.1',
-    'requests==2.9.1'
-)
-TEST_REQUIREMENTS = (
-)
-
 
 setup(
-    name="adyengo",
-    version=VERSION,
-    author="Rik",
-    author_email="gitaarik@gmail.com",
-    description="Integrate Adyen in Django.",
-    long_description=open('README.md', 'rt').read(),
-    url="https://github.com/gitaarik/adyengo",
+    name='adyengo',
+    keywords='django adyen payment integration',
+    version='0.0.1',
+    author='gitaarik',
+    author_email='gitaarik@gmail.com',
+    description='Django app for easy Adyen integration.',
+    url='https://github.com/gitaarik/adyengo',
+    license='LGPL licence, see LICENCE.txt',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=REQUIREMENTS,
-    tests_require=TEST_REQUIREMENTS,
+    install_requires=('ipaddress==1.0.4', 'python-dateutil==2.1', 'requests==2.9.1'),
     zip_safe=False,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Lesser General Public License v3.0',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Framework :: Django',
-    ]
 )
